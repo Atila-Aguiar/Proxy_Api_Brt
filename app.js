1ccfd7a83b6b31999f91748ebaf169949c9c855d
Atila_Aguiar <atilasilva007@gmail.com>
2024-08-20 18:55:07 -0300

    Primeiro commit subindo os arquivos

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios')
+
+const app = express();
+
+app.use(cors());
+var onibus_linhas_importantes = []
+setInterval(async () => {
+  let url = "https://dados.mobilidade.rio/gps/brt"
+    const response = await axios.get(url)
+    onibus_linhas_importantes = response.data.veiculos.filter(x => parseInt(x.linha) != 0 && x.ignicao != 0 && x.velocidade != 0)
+}, 20000);
+
+app.get('/', async (req, res) => {
+    res.json(onibus_linhas_importantes);
+});
+
+app.listen(5000, () => {
+  console.log('Servidor rodando na porta 5000');
+});
