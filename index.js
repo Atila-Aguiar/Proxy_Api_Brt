afdad6f3154160b93433832ccd85693eba766229
Atila_Aguiar <atilasilva007@gmail.com>
2024-08-21 09:46:44 -0300

    Mudando rota do vercel

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const axios = require('axios')
 const app = express();
 
 app.use(cors());
-var onibus_linhas_importantes = []
-setInterval(async () => {
-  let url = "https://dados.mobilidade.rio/gps/brt"
-    const response = await axios.get(url)
-    onibus_linhas_importantes = response.data.veiculos.filter(x => parseInt(x.linha) != 0 && x.ignicao != 0 && x.velocidade != 0)
-}, 20000);
+// var onibus_linhas_importantes = []
+// setInterval(async () => {
+//   let url = "https://dados.mobilidade.rio/gps/brt"
+//   const response = await axios.get(url)
+//   onibus_linhas_importantes = response.data.veiculos.filter(x => parseInt(x.linha) != 0 && x.ignicao != 0 && x.velocidade != 0)
+// }, 20000);
 
 app.get('/', async (req, res) => {
-    res.json(onibus_linhas_importantes);
+  let url = "https://dados.mobilidade.rio/gps/brt"
+  const response = await axios.get(url)
+  let onibus_linhas_importantes = response.data.veiculos.filter(x => parseInt(x.linha) != 0 && x.ignicao != 0 && x.velocidade != 0)
+  res.json(onibus_linhas_importantes);
 });
 
 app.listen(process.env.PORT || 5000, () => {
