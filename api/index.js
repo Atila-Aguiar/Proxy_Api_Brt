75fa0f8760c47b1d1bcfaf92b73da90ba3ce4a6d
Atila_Aguiar <atilasilva007@gmail.com>
2024-08-20 21:57:28 -0300

    Configurações do vercel

--- api/index.js
+++ api/index.js
@@ -17,5 +17,5 @@ app.get('/', async (req, res) => {
 });
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log('Servidor rodando na porta 5000');
+  console.log('Servidor rodando na porta ', process.env.PORT || 5000);
 });
